59885419d8bc48de527e1b8dff95b6b107e3e1c5
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-10-02 15:04:43 +0800

    Day 69 - C1: select preset JavaScript from action sheet

--- Project-19/Extension/ActionViewController.swift
+++ Project-19/Extension/ActionViewController.swift
@@ -11,17 +11,48 @@ import MobileCoreServices
 class ActionViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-
+    @IBOutlet var script: UITextView!
+    
+    var pageTitle = ""
+    var pageURL = ""
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
     
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addScript))
+        
+        let notificationCenter = NotificationCenter.default
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+            
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             
-            if let itemProvider = inputItem.attachments?.first {
-                
-                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
-                     
-                    // do stuff
+                if let itemProvider = inputItem.attachments?.first {
+                    
+                    itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+                        
+                        // do stuff!
+                        
+                        guard let itemDictionary = dict as? NSDictionary else { return }
+                        
+                        guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                        
+                        //print(javaScriptValues)
+                        
+                        self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                        
+                        self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                        
+                        DispatchQueue.main.async {
+                            
+                            self?.title = self?.pageTitle
+                            
+                        }
                     
                 }
             }
@@ -31,7 +62,48 @@ class ActionViewController: UIViewController {
     @IBAction func done() {
         // Return any edited content to the host app.
         // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text!]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    // Day 69 - Challenge 1: use UIAlertController to allow users to select prewritten example scripts
+    @objc func addScript() {
+        let ac = UIAlertController(title: "Scipts", message: "Select Script", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Title", style: .default, handler: { action in
+            self.script.text = "alert(document.title);"
+            
+        }))
+        ac.addAction(UIAlertAction(title: "URL", style: .default, handler: { action in
+            self.script.text = "alert(document.URL);"
+            
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        script.scrollIndicatorInsets = script.contentInset
+
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
     }
 
 }
