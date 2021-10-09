c3004b8b5789f4a29ea588cb0eb7f943eb78b239
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-10-09 16:18:56 +0800

    update

--- Project-19/Extension/ActionViewController.swift
+++ Project-19/Extension/ActionViewController.swift
@@ -22,7 +22,7 @@ class ActionViewController: UIViewController {
     
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addScript))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(optionAlert))
         
         let notificationCenter = NotificationCenter.default
         
@@ -73,18 +73,39 @@ class ActionViewController: UIViewController {
     }
     
     // Day 69 - Challenge 1: use UIAlertController to allow users to select prewritten example scripts
-    @objc func addScript() {
-        let ac = UIAlertController(title: "Scipts", message: "Select Script", preferredStyle: .actionSheet)
+    @objc func optionAlert() {
+        
+        let ac = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Examples", style: .default, handler: examples))
+        ac.addAction(UIAlertAction(title: "Load", style: .default, handler: examples))
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func save(action: UIAlertAction) {
+        
+    }
+    
+    func examples(action: UIAlertAction) {
+        
+        let ac = UIAlertController(title: "Example Scripts", message: nil, preferredStyle: .alert)
+
         ac.addAction(UIAlertAction(title: "Title", style: .default, handler: { action in
             self.script.text = "alert(document.title);"
-            
+        
         }))
         ac.addAction(UIAlertAction(title: "URL", style: .default, handler: { action in
             self.script.text = "alert(document.URL);"
-            
+        
         }))
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
+
+        
+        
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
